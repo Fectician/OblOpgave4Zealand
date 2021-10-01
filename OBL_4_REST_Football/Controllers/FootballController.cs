a370241fc74289ac4ab81e2e4912bfeb6a0edcd7
Fectician <45406510+Fectician@users.noreply.github.com>
2021-10-01 15:31:19 +0200

    Add files via upload

---
+++ OBL_4_REST_Football/Controllers/FootballController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OBL_4_REST_Football.Managers;
+using OBL_4_REST_Football.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OBL_Opgave_4_Rest.Controllers
+{
+    //Kode af Nicolas Lauridsen, Datamatiker 3b Zealand Erhvervsakademi.
+    [Route("FootballPlayer")]
+    [ApiController]
+    public class FootballController : ControllerBase
+    {
+        private readonly FootballManager _manager = new FootballManager();
+        // GET: api/<ItemsController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<ItemsController>/5
+        [HttpGet("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<ItemsController>/5
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ OBL_4_REST_Football/Managers/FootballManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OBL_4_REST_Football.Models;
+
+namespace OBL_4_REST_Football.Managers
+{
+    public class FootballManager
+    {
+        //Kode af Nicolas Lauridsen, Datamatiker 3b Zealand Erhvervsakademi.
+        private static int _nextId = 1;
+
+        private static List<FootballPlayer> Data = new List<FootballPlayer>
+        {
+            new FootballPlayer {Id = _nextId++, Price = 48, Name = "Carlos", ShirtNumber = 6},
+            new FootballPlayer {Id = _nextId++, Price = 44, Name = "Steve", ShirtNumber = 8}
+
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Data.Find(footballplayer => footballplayer.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newFootballPlayer)
+        {
+            newFootballPlayer.Id = _nextId++;
+            Data.Add(newFootballPlayer);
+            return newFootballPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer player = Data.Find(player1 => player1.Id == id);
+            if (player == null) return null;
+            Data.Remove(player);
+            return player;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer player = Data.Find(player1 => player1.Id == id);
+            if (player == null) return null;
+            player.Name = updates.Name;
+            player.Price = updates.Price;
+            player.ShirtNumber = updates.ShirtNumber;
+            return player;
+        }
+    }
+}
