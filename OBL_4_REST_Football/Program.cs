1b7725304ff8dde03d6ea5f44c4e40c5f892ae84
Fectician <45406510+Fectician@users.noreply.github.com>
2021-10-01 15:33:06 +0200

    Add files via upload

---
+++ OBL_4_REST_Football/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OBL_4_REST_Football
+{
+    public class Program
+    {
+		//Kode af Nicolas Lauridsen, Datamatiker 3b Zealand Erhvervsakademi.
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
